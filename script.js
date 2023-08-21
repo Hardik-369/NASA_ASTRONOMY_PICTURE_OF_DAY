1c06f81de2268b930eb9ff1e89e35ee5c68fdaac
Hardik-369 <142771505+Hardik-369@users.noreply.github.com>
2023-08-21 17:45:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const form = document.getElementById('date-form');
+const imageInfo = document.getElementById('image-info');
+
+form.addEventListener('submit', async function (event) {
+  event.preventDefault();
+
+  const dateInput = form.elements.date.value;
+  const apiKey = 'akfcyWogmlDXFt1usDBV9UabJINO7TBitGFb20cI'; // Replace with your NASA API key
+
+  const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${dateInput}`;
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    imageInfo.innerHTML = `
+      <h2>${data.title}</h2>
+      <p>${data.explanation}</p>
+      <img src="${data.url}" alt="${data.title}">
+    `;
+
+    imageInfo.style.display = 'block'; // Display the box
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    imageInfo.innerHTML = '<p>Error fetching APOD image data.</p>';
+  }
+});
